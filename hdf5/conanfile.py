ef1ec1067e00964de0715d9f77842098152527c8
Kai Wolf <mail@kai-wolf.me>
2019-10-25 15:30:36 +0200

    Downgrade HDF5 to 1.8.21

--- hdf5/conanfile.py
+++ hdf5/conanfile.py
@@ -3,7 +3,7 @@ from conans.tools import download, unzip
 
 class Hdf5Conan(ConanFile):
     name = "hdf5"
-    version = "1.10.5"
+    version = "1.8.21"
     license = "BSD-style Open Source"
     url = "https://support.hdfgroup.org/ftp/HDF5/releases"
     author = "The HDF Group"
@@ -12,14 +12,12 @@ class Hdf5Conan(ConanFile):
     generators = "cmake"
 
     def source(self):
-        # https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-1.10.5/src/CMake-hdf5-1.10.5.zip
-        # https://support.hdfgroup.org/ftp/HDF5/releases
-        pkg_name = "CMake-hdf5-1.10.5.zip"
-        download(self.url + "/hdf5-1.10/hdf5-1.10.5/src/" + pkg_name, pkg_name)
+        pkg_name = "CMake-hdf5-1.8.21.zip"
+        download(self.url + "/hdf5-1.8/hdf5-1.8.21/src/" + pkg_name, pkg_name)
         unzip(pkg_name)
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(source_folder="CMake-hdf5-1.10.5/hdf5-1.10.5")
+        cmake.configure(source_folder="CMake-hdf5-1.8.21/hdf5-1.8.21")
         cmake.build()
         cmake.install()
