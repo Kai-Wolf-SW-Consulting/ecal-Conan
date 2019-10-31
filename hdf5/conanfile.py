8ee97c3976a207b8c091b0871e0aa6a79620c962
Kai Wolf <kai.wolf@gmail.com>
2019-10-31 15:16:38 +0100

    Add cmake patch for HDF5 package

--- hdf5/conanfile.py
+++ hdf5/conanfile.py
@@ -1,5 +1,6 @@
 from conans import ConanFile, CMake
-from conans.tools import download, unzip
+from conans.tools import download, get, patch, unzip
+
 
 class Hdf5Conan(ConanFile):
     name = "hdf5"
@@ -7,14 +8,19 @@ class Hdf5Conan(ConanFile):
     license = "BSD-style Open Source"
     url = "https://support.hdfgroup.org/ftp/HDF5/releases"
     author = "The HDF Group"
+    topics = ("c++", "I/O", "File format")
     description = "High-performance data management and storage library"
-    settings = "os", "compiler", "build_type", "arch"
+    settings = "os", "compiler", "arch"
+    exports_sources = "cmake_config_folder.patch"
     generators = "cmake"
 
     def source(self):
         pkg_name = "CMake-hdf5-1.8.21.zip"
         download(self.url + "/hdf5-1.8/hdf5-1.8.21/src/" + pkg_name, pkg_name)
         unzip(pkg_name)
+        patch(base_path="CMake-hdf5-1.8.21/hdf5-1.8.21",
+              patch_file="cmake_config_folder.patch",
+              strip=1)
 
     def build(self):
         cmake = CMake(self)
